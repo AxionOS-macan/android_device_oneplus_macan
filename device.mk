38a2efd7acd5d4bb2019675723ad7651c4067a3c
chandu078 <chandudyavanapelli03@gmail.com>
2026-03-08 10:45:02 +0000

    macan: Switch to richtap vibrator
    Change-Id: Iafceef4fc6c4a293883595a8e1cf2d9e44b0651e

--- device.mk
+++ device.mk
@@ -66,10 +66,7 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-$(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
-$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oplus.macan)
+    android.hardware.vibrator.service.oplus-richtap
 
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm8850-common/common.mk)
